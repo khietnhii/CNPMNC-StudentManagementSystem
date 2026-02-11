479fa462246bdbe21c295d27578ddcffa81f3070
Khanh An <khanhan501@gmail.com>
2026-02-11 15:13:57 +0700

    update entity & repository

---
+++ src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package vn.edu.hcmut.cse.adsoftweng.lab.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vn.edu.hcmut.cse.adsoftweng.lab.entity.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
+    // JpaRepository<Student, String> có nghĩa là:
+    // - Student: Thực thể cần quản lý
+    // - String: Kiểu dữ liệu của khóa chính (@Id)
+}
